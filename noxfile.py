f88f621d30bf3b3fdad8b598ec817de6ddf107e9
Tim Sweña (Swast) <tswast@gmail.com>
2026-08-13 13:36:08 -0500

    use 3.14 for unit prerelease and noextras

--- noxfile.py
+++ noxfile.py
@@ -60,7 +60,8 @@ LINT_PATHS = [
 
 DEFAULT_PYTHON_VERSION = "3.14"
 
-ALL_PYTHON = ["3.10", "3.11", "3.12", "3.13", "3.14", "3.15"]
+# TODO(tswast): include Python 3.15 when there are pyarrow wheels.
+ALL_PYTHON = ["3.10", "3.11", "3.12", "3.13", "3.14"]
 UNIT_TEST_STANDARD_DEPENDENCIES = [
     "mock",
     PYTEST_VERSION,

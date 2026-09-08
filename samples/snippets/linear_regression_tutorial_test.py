21b95c7873515479d2dd8e978d5b89c5447293c8
Sindhu Vydana <svydana@google.com>
2026-09-08 13:05:24 -0700

    docs: update BigQuery DataFrames ML(linear regression) tutorial to use the bigframes.bigquery.ml API.
    BUG: b/555917558
    Change-Id: If6c134e34b368dac94b518388dc6eabf2fbad1ae
    Reviewed-on: https://bigframes-internal-review.git.corp.google.com/c/bigframes/+/3800
    Kokoro-Prerelease: Kokoro <noreply+kokoro-dedicatedkokoro-dedicated Kokorogoogle.com>
    Reviewed-by: Tim Swena <swast@google.com>
    Kokoro-Unit: Kokoro <noreply+kokoro-dedicatedkokoro-dedicated Kokorogoogle.com>
    Kokoro-Docs: Kokoro <noreply+kokoro-dedicatedkokoro-dedicated Kokorogoogle.com>
    Kokoro-Lint: Kokoro <noreply+kokoro-dedicatedkokoro-dedicated Kokorogoogle.com>
    Kokoro-System: Kokoro <noreply+kokoro-dedicatedkokoro-dedicated Kokorogoogle.com>
    Kokoro-Doctest: Kokoro <noreply+kokoro-dedicatedkokoro-dedicated Kokorogoogle.com>
    Reviewed-by: Shenyang Cai <sycai@google.com>

--- samples/snippets/linear_regression_tutorial_test.py
+++ samples/snippets/linear_regression_tutorial_test.py
@@ -16,8 +16,13 @@
 def test_linear_regression(random_model_id: str) -> None:
     your_model_id = random_model_id
     # [START bigquery_dataframes_bqml_linear_regression]
-    from bigframes.ml.linear_model import LinearRegression
     import bigframes.pandas as bpd
+    from bigframes.bigquery import ml
+
+    # Set partial ordering mode for BigQuery DataFrames.
+    # For more information, see the BigQuery DataFrames performance documentation:
+    # https://cloud.google.com/bigquery/docs/dataframes-performance#partial-ordering-mode
+    bpd.options.bigquery.ordering_mode = "partial"
 
     # Load data from BigQuery
     bq_df = bpd.read_gbq("bigquery-public-data.ml_datasets.penguins")
@@ -25,52 +30,50 @@ def test_linear_regression(random_model_id: str) -> None:
     # Drop rows with nulls to get training data
     training_data = bq_df.dropna(subset=["body_mass_g"])
 
-    # Specify your feature (or input) columns and the label (or output) column:
-    feature_columns = training_data.drop(columns=["body_mass_g"])
-    label_columns = training_data[["body_mass_g"]]
-
-    # Create the linear model
-    model = LinearRegression()
-    model.fit(feature_columns, label_columns)
-    model.to_gbq(
-        your_model_id,  # For example: "bqml_tutorial.penguins_model"
+    # A Linear Regression model predicts a continuous numerical value.
+    #
+    # Use ml.create_model to create and train the model in BigQuery.
+    # The options parameter specifies the model type and the label column.
+    # For more information, see the BigQuery DataFrames API reference documentation:
+    # https://dataframes.bigquery.dev/reference/api/bigframes.bigquery.ml.create_model.html#bigframes.bigquery.ml.create_model
+    ml.create_model(
+        your_model_id,  # For example: "bqml_tutorial.penguins_model",
+        options={
+            "model_type": "LINEAR_REG",
+            "input_label_cols": ["body_mass_g"],
+        },
+        training_data=training_data,
         replace=True,
     )
     # [END bigquery_dataframes_bqml_linear_regression]
     # [START bigquery_dataframes_bqml_linear_evaluate]
-    import bigframes.pandas as bpd
-
-    # Select the model you will be evaluating. `read_gbq_model` loads model data from
-    # BigQuery, but you could also use the `model` object from the previous steps.
-    model = bpd.read_gbq_model(
-        your_model_id,  # For example: "bqml_tutorial.penguins_model"
+    # Use the ml.evaluate method to evaluate the model with test data.
+    # For more information, see the BigQuery DataFrames API reference documentation:
+    # https://dataframes.bigquery.dev/reference/api/bigframes.bigquery.ml.evaluate.html#bigframes.bigquery.ml.evaluate
+    ml.evaluate(
+        your_model_id,  # For example: "bqml_tutorial.penguins_model",
+        input_=training_data,
     )
-
-    # Score the model with input data defined in an earlier step to compare
-    # model predictions on feature_columns to true labels in label_columns.
-    score = model.score(feature_columns, label_columns)
     # Expected output results:
     # index  mean_absolute_error  mean_squared_error  mean_squared_log_error  median_absolute_error  r2_score  explained_variance
     #   0        227.012237         81838.159892            0.00507                173.080816        0.872377    0.872377
     #   1 rows x 6 columns
     # [END bigquery_dataframes_bqml_linear_evaluate]
     # [START bigquery_dataframes_bqml_linear_predict]
-    # Select the model you'll use for predictions. `read_gbq_model` loads
-    # model data from BigQuery, but you could also use the `model` object
-    # object from previous steps.
-    model = bpd.read_gbq_model(
-        your_model_id,
-        # For example: "bqml_tutorial.penguins_model",
-    )
-
     # Load data from BigQuery
     bq_df = bpd.read_gbq("bigquery-public-data.ml_datasets.penguins")
 
     # Use 'contains' function to filter by island containing the string
     # "Biscoe".
-    biscoe_data = bq_df.loc[bq_df["island"].str.contains("Biscoe")]
+    biscoe_data = bq_df[bq_df["island"].str.contains("Biscoe")]
 
-    result = model.predict(biscoe_data)
+    # Use the ml.predict method to predict results using your model.
+    # For more information, see the BigQuery DataFrames API reference documentation:
+    # https://dataframes.bigquery.dev/reference/api/bigframes.bigquery.ml.predict.html#bigframes.bigquery.ml.predict
+    ml.predict(
+        your_model_id,  # For example: "bqml_tutorial.penguins_model",
+        input_=biscoe_data,
+    )
 
     # Expected output results:
     #     predicted_body_mass_g  	      species	                island	 culmen_length_mm  culmen_depth_mm   body_mass_g 	flipper_length_mm	sex
@@ -79,10 +82,18 @@ def test_linear_regression(random_model_id: str) -> None:
     # 160	  4731.310452	   Gentoo penguin (Pygoscelis papua)	Biscoe	      44.5	            14.3	        216.0	          4100.0	    <NA>
     # [END bigquery_dataframes_bqml_linear_predict]
     # [START bigquery_dataframes_bqml_linear_predict_explain]
-    # Use 'predict_explain' function to understand why the model is generating these prediction results.
-    # 'predict_explain'is an extended version of the 'predict' function that not only outputs prediction results, but also outputs additional columns to explain the prediction results.
-    # Using the trained model and utilizing data specific to Biscoe Island, explain the predictions of the top 3 features
-    explained = model.predict_explain(biscoe_data, top_k_features=3)
+    # Use the ml.explain_predict method to understand why the model is
+    # generating these prediction results.
+    # For more information, see the BigQuery DataFrames API reference documentation:
+    # https://dataframes.bigquery.dev/reference/api/bigframes.bigquery.ml.explain_predict.html#bigframes.bigquery.ml.explain_predict
+    #
+    # Using the trained model and utilizing data specific to Biscoe Island,
+    # explain the predictions of the top 3 features.
+    ml.explain_predict(
+        your_model_id,  # For example: "bqml_tutorial.penguins_model",
+        input_=biscoe_data,
+        top_k_features=3,
+    )
 
     # Expected results:
     #   predicted_body_mass_g               top_feature_attributions	        baseline_prediction_value	prediction_value	approximation_error	              species	            island	culmen_length_mm	culmen_depth_mm	flipper_length_mm	body_mass_g	    sex
@@ -93,18 +104,31 @@ def test_linear_regression(random_model_id: str) -> None:
     # 4	 4637.165037	        [{'feature': 'island', 'attribution': 7348.877...	-5320.222128	          4637.165037	            0.0	         Gentoo penguin (Pygoscelis papua)	Biscoe	    46.1	              13.2	        211.0	           4500.0	   FEMALE
     # [END bigquery_dataframes_bqml_linear_predict_explain]
     # [START bigquery_dataframes_bqml_linear_global_explain]
-    # To use the `global_explain()` function, the model must be recreated with `enable_global_explain` set to `True`.
-    model = LinearRegression(enable_global_explain=True)
-
-    # The model must the be fitted before it can be saved to BigQuery and then explained.
+    # To use the ml.global_explain method, the model must be created with
+    # enable_global_explain set to True.
+    #
+    # Use ml.create_model to create and train the model in BigQuery.
+    # The options parameter specifies the model type and the label column.
+    # For more information, see the BigQuery DataFrames API reference documentation:
+    # https://dataframes.bigquery.dev/reference/api/bigframes.bigquery.ml.create_model.html#bigframes.bigquery.ml.create_model
     training_data = bq_df.dropna(subset=["body_mass_g"])
-    X = training_data.drop(columns=["body_mass_g"])
-    y = training_data[["body_mass_g"]]
-    model.fit(X, y)
-    model.to_gbq("bqml_tutorial.penguins_model", replace=True)
+    ml.create_model(
+        your_model_id,  # For example: "bqml_tutorial.penguins_model",
+        options={
+            "model_type": "LINEAR_REG",
+            "input_label_cols": ["body_mass_g"],
+            "enable_global_explain": True,
+        },
+        training_data=training_data,
+        replace=True,
+    )
 
-    # Explain the model
-    explain_model = model.global_explain()
+    # Use the ml.global_explain method to explain the model.
+    # For more information, see the BigQuery DataFrames API reference documentation:
+    # https://dataframes.bigquery.dev/reference/api/bigframes.bigquery.ml.global_explain.html#bigframes.bigquery.ml.global_explain
+    ml.global_explain(
+        your_model_id,  # For example: "bqml_tutorial.penguins_model",
+    )
 
     # Expected results:
     #                       attribution
@@ -116,10 +140,3 @@ def test_linear_regression(random_model_id: str) -> None:
     # culmen_depth_mm	    117.084944
     # culmen_length_mm	    94.366793
     # [END bigquery_dataframes_bqml_linear_global_explain]
-    assert explain_model is not None
-    assert feature_columns is not None
-    assert label_columns is not None
-    assert model is not None
-    assert score is not None
-    assert result is not None
-    assert explained is not None
